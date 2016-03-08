aa521bda64532285aa9493b328da106702fef6e9
vanshkumar <vanshkumar95@gmail.com>
2016-03-08 14:17:27 -0800

    Finished EM

--- train.py
+++ train.py
@@ -12,40 +12,29 @@ def latex_matrix(matrix):
     return matrix_str
 
 
-# Uses a single Maximization step to compute A (state-transition) and
-# O (observation) matrices. See Lecture 6 Slide 65.
-def MStep(pos_states, pos_observations, state_seq, obs_seq):
+# E[i, j] = P(y_i = j), shape is M x L
+# m is emission.shape[1] (size of observation space)
+def MStep(state_space, observs, E, m):
+    L = len(state_space)
 
-    A = [[1. / len(pos_states) for i in pos_states] for j in pos_states]
-    O = [[1. / len(pos_observations) for i in pos_observations]
-         for j in pos_states]
+    transition = np.zeros((L, L))
+    emission   = np.zeros((L, m))
 
-    # create transition matrix
-    for prev_state in pos_states:
-        for state in pos_states:
-            num = 0.0
-            den = 0.0
-            for j in range(len(state_seq) - 1):
-                if (state_seq[j] == pos_states[prev_state]):
-                    den += 1
-                    if state_seq[j + 1] == pos_states[state]:
-                        num += 1
-            A[pos_states[state]][pos_states[prev_state]] = num / \
-                den if den != 0 else 0
+    # Calculate transition and emission matrix
+    for i in range(L):
+        # Normalization factor for both matrices
+        norm = np.sum(E[:, i])
 
-    # create observation matrix
-    for state in pos_states:
-        for obs in pos_observations:
-            num = 0.0
-            den = 0.0
-            num = sum([int(obs_seq[j] == pos_observations[obs]) and
-                       int(state_seq[j] == pos_states[state]) for j in range(len(state_seq))])
-            den = sum([int(state_seq[j] == pos_states[state])
-                       for j in range(len(state_seq))])
+        for j in range(M-1):
+            transition[i, :] += E[i, j] * E[:, j+1].T
+        transition[i, :] /= norm
 
-            O[pos_states[state]][pos_observations[obs]] = float(num) / den
+        for j in range(M):
+            val = observs[j] # jth emission in sequence
+            emission[i, val] += E[i, val]
+        emission[i, :] /= norm
 
-    return A, O
+    return transition, emission
 
 
 def EStep(state_space, observs, transition, emission):
@@ -54,8 +43,8 @@ def EStep(state_space, observs, transition, emission):
     L = len(state_space)
 
     # Run fwd-bckwd with uniform start probabilities
-    uniform = [1./L  for i in range(L)]
-    fwd_probs = forward(state_space, uniform, observs, transition, emission)
+    uniform     = [1./L  for i in range(L)]
+    fwd_probs   = forward(state_space, uniform, observs, transition, emission)
     bckwd_probs = backward(state_space, observs, transition, emission)
 
     # Calculate P(y_i) for each y = (y1, ..., yM)
@@ -65,7 +54,7 @@ def EStep(state_space, observs, transition, emission):
         dotprod = np.dot(fwd_probs[:, i].T, bckwd_probs[:, i])
         E[:, i] = fwd_probs[:, i] * bckwd_probs[:, i] / dotprod
     
-    return E
+    return E.T
 
 
 # state_space is all possible states x (length L)
@@ -99,63 +88,9 @@ def backward(state_space, observs, transition, emission):
     for i in reversed(range(M)):
         bwd_prev = np.dot(transition, np.diag(emission[:, observs[i]]))
         bwd_prev = np.dot(bwd_prev, bwd_probs[:,0])
-        
+
         # append in the opposite order so that the prev column is always in the 
         # front
         bwd_probs = np.append(bwd_prev, bwd_probs, 1)
         
     return bwd_probs
-
-
-def Forward(num_states, obs, A, O):
-    """Computes the probability a given HMM emits a given observation using the
-        forward algorithm. This uses a dynamic programming approach, and uses
-        the 'prob' matrix to store the probability of the sequence at each length.
-        Arguments: num_states the number of states
-                   obs        an array of observations
-                   A          the transition matrix
-                   O          the observation matrix
-        Returns the probability of the observed sequence 'obs'
-    """
-    len_ = len(obs)                   # number of observations
-    # stores p(seqence)
-    prob = [[[0.] for i in range(num_states)] for i in range(len_)]
-    
-    # initializes uniform state distribution, factored by the
-    # probability of observing the sequence from the state (given by the
-    # observation matrix)
-    prob[0] = [(1. / num_states) * O[j][obs[0]] for j in range(num_states)]
-    # We iterate through all indices in the data
-    for length in range(1, len_):   # length + 1 to avoid initial condition
-        for state in range(num_states):
-            # stores the probability of transitioning to 'state'
-            p_trans = 0
-
-            # probabilty of observing data in our given 'state'
-            p_obs = O[state][obs[length]]
-
-            # We iterate through all possible previous states, and update
-            # p_trans accordingly.
-            for prev_state in range(num_states):
-                p_trans += prob[length - 1][prev_state] * A[prev_state][state]
-
-            prob[length][state] = p_trans * p_obs  # update probability
-
-        prob[length] = prob[length][:]  # copies by value
-    # start backwards code
-    prob_back = [[0.] for i in range(len_)]    
-    prob_back[-1] = [1 for j in range(num_states)]    
-    for length in reversed(range(0, len_ - 1)):
-        for state in range(num_states):
-            prob_back[length][state] = sum([prob[length + 1][j] * A[j][state] * O[j][obs[length]] for j in range(num_states)])
-    # end backwards code
-    # start finding the actual probabilities
-    P = [[[0.] for i in range(num_states)] for i in range(len_)]
-    
-    for length in range(len_):
-        for state in range(num_states):
-            P[length][state] = prob[length][state] * prob_back[length][state] / \
-                sum([prob[length][j] * prob_back[length][j] for j in range(num_states)])
-    # end computing the probabilities
-    # return total probability
-    return P
